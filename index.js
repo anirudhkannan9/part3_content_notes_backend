e48975a211e7819988308a2b01f32ecf0f45542b
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-05-06 14:47:14 -0700

    uibuild

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.get('/api/notes/:id', (request, response, next) => {
     
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -106,9 +106,11 @@ app.post('/api/notes', (request, response) => {
 
   note
     .save()
-    .then(savedNote => {
-      response.json(savedNote)
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => {
+      response.json(savedAndFormattedNote)
     })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
@@ -153,6 +155,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
--- models/note.js
+++ models/note.js
@@ -20,8 +20,15 @@ mongoose
 
 //define schema for a note and matching model
 const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
+    content: {
+        type: String,
+        minlength: 5,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
     important: Boolean
 })
 
